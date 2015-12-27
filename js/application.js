a97bf88b69854f3c63660dcc7448caf76e634065
Masaki iida <iida@whizzy.co.jp>
2015-12-27 13:09:28 +0900

    Implemented speech recognition

---
+++ js/application.js
@@ -0,0 +1,45 @@
+var recognition;
+var nowRecognition = false;
+var $finalSpan = document.querySelector('#final_span');
+var $interimSpan = document.querySelector('#interim_span');
+
+function start () {
+	recognition = new webkitSpeechRecognition();
+	//recognition.lang = document.querySelector('#select2').value;
+	recognition.lang = 'ja-JP';
+	recognition.continuous = true;
+	recognition.interimResults = true;
+	recognition.onresult = function (e) {
+		var finalText = '';
+		var interimText = '';
+		for (var i = 0; i < e.results.length; i++) {
+			if (e.results[i].isFinal) {
+				finalText += e.results[i][0].transcript;
+				console.log(finalText);
+			} else {
+				interimText += e.results[i][0].transcript;
+			}
+		}
+		$interimSpan.textContent = interimText;
+		$finalSpan.textContent = finalText;
+	};
+	recognition.start();
+	nowRecognition = true;
+};
+
+function stop () {
+	recognition.stop();
+	nowRecognition = false;
+}
+
+document.querySelector('#btn2').onclick = function () {
+	if (nowRecognition) {
+		stop();
+		this.value = '音声認識を継続的に行う';
+		this.className = 'btn btn-lg btn-primary';
+	} else {
+		start();
+		this.value = '音声認識を止める';
+		this.className = 'btn btn-lg btn-primary';
+	}
+}
